66b1af79a61a54a7cc1f65362a4828a614536e06
Damian <damian.hercun@gmail.com>
2014-01-30 23:09:25 +0100

    LoginWindow avec selection de partie

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import view.Window;
+import view.LoginWindow;
 
 import javax.swing.*;
 
@@ -12,7 +12,7 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new Window();
+                new LoginWindow();
             }
         });
     }
